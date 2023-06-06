069ac00ccd2e669dfc7becba9ab0cedbc3700526
Shichiro Nakahara <shichiro.nakahara@proton.me>
2023-06-07 11:07:55 +1200

    Update pipeline panic message to handle error objects.

--- yarn-project/falafel/src/world_state.ts
+++ yarn-project/falafel/src/world_state.ts
@@ -334,7 +334,7 @@ export class WorldState {
       this.log(err);
 
       let errorMessage = `\u{1F6A8} PIPELINE PANIC!`;
-      errorMessage += `\n\n${err}`;
+      errorMessage += `\n\n${typeof err == 'object' ? JSON.stringify(err) : err}`;
       this.notifier.send(errorMessage);
     });
   }
