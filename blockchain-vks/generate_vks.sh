6df3a38199cc772cf1144d44a0370a5288e2afb4
Shichiro Nakahara <shichiro.nakahara@proton.me>
2024-04-15 09:47:31 +0000

    * Reverting upstream changes to VerificationKey28x32.sol * Update blockchain-vks/generate_vks.sh to generate production keys

--- blockchain-vks/generate_vks.sh
+++ blockchain-vks/generate_vks.sh
@@ -5,11 +5,10 @@ OUTPUT_DIR=../../blockchain-vks/keys
 
 cd ../aztec-connect-cpp/
 
-(cd barretenberg/cpp/srs_db && ./download_ignition.sh 1)
+(cd barretenberg/cpp/srs_db && ./download_ignition.sh 10)
 
 cd build
 
 mkdir -p $OUTPUT_DIR
 
-./bin/keygen 1 1 $OUTPUT_DIR true 2> >(awk '$0="mock: "$0' 1>&2)
-./bin/keygen 1 1 $OUTPUT_DIR 2> >(awk '$0="real1x1: "$0' 1>&2)
+./bin/keygen 28 32 $OUTPUT_DIR 2> >(awk '$0="real28x32: "$0' 1>&2)
